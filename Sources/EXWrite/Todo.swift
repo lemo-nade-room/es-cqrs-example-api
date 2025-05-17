33fc70a07dcaf24f50297ed4932f98ffba8bccd7
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-05-17 23:33:23 +0900

    chore: 🔨 #14 Todo集約にSnapshotを追加

--- Sources/EXWrite/Todo.swift
+++ Sources/EXWrite/Todo.swift
@@ -1,12 +1,27 @@
 package import EXEvent
+import Foundation
 
 package actor Todo {
     package nonisolated let aid: TodoID
+    var seqNr: Int
+    package nonisolated let version: Int
+    var lastUpdatedAt: Date
+
     var title: Title
     var description: Description
 
-    init(aid: TodoID, title: Title, description: Description) {
+    init(
+        aid: TodoID,
+        seqNr: Int,
+        version: Int,
+        lastUpdatedAt: Date,
+        title: Title,
+        description: Description
+    ) {
         self.aid = aid
+        self.seqNr = seqNr
+        self.version = version
+        self.lastUpdatedAt = lastUpdatedAt
         self.title = title
         self.description = description
     }
