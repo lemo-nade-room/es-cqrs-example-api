4ed24181cc1d5f1664a0a684e9dfadbc2ee1da01
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-05-17 18:18:09 +0900

    Swift package init

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-cqrs-example-api",
+    platforms: [.macOS(.v15)],
+    targets: [
+        .target(
+            name: "EXWebAPI",
+            swiftSettings: swiftSettings,
+        ),
+        .testTarget(
+            name: "EXWebAPITests",
+            dependencies: [
+                .target(name: "EXWebAPI")
+            ],
+            swiftSettings: swiftSettings,
+        ),
+    ],
+)
+var swiftSettings: [SwiftSetting] {
+    [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+    ]
+}
