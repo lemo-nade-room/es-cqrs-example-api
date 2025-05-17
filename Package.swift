ac9ae1256e39fd71f04ea9a0facc4dda148f4199
Lemo-nade-room <lemo.nade.engineer.room@gmail.com>
2025-05-17 20:09:16 +0900

    chore: 🔨 #4 EXExternalAPITestsターゲットを作成し、OpenAPIGeneratorを使用するための依存関係を設定

--- Package.swift
+++ Package.swift
@@ -6,6 +6,14 @@ import PackageDescription
 let package = Package(
     name: "swift-cqrs-example-api",
     platforms: [.macOS(.v15)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.0.0"),
+        .package(
+            url: "https://github.com/swift-server/swift-openapi-async-http-client.git",
+            from: "1.0.0",
+        ),
+    ],
     targets: [
         .target(
             name: "EXWebAPI",
@@ -23,6 +31,24 @@ let package = Package(
         .target(
             name: "EXTestUtil",
             swiftSettings: swiftSettings,
+        ),
+
+        // MARK: External API Test
+        .testTarget(
+            name: "EXExternalAPITests",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(
+                    name: "OpenAPIAsyncHTTPClient",
+                    package: "swift-openapi-async-http-client"
+                ),
+
+                .target(name: "EXTestUtil"),
+            ],
+            swiftSettings: swiftSettings,
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ],
         ),
     ],
 )
